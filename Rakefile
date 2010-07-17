1f8f470d7788ba7a659253d57207656335828536
aberant <qzzzq1@gmail.com>
2010-07-17 11:54:26 -0500

    updating readme, adding MIT license, adding rdoc task

--- Rakefile
+++ Rakefile
@@ -8,3 +8,17 @@ Spec::Rake::SpecTask.new do |t|
   t.rcov = false
   t.spec_opts = ["--colour"]
 end
+
+require 'rake/rdoctask'
+Rake::RDocTask.new do |rdoc|
+  if File.exist?('VERSION')
+    version = File.read('VERSION')
+  else
+    version = ""
+  end
+
+  rdoc.rdoc_dir = 'rdoc'
+  rdoc.title = "Measurement #{version}"
+  rdoc.rdoc_files.include('README*')
+  rdoc.rdoc_files.include('lib/**/*.rb')
+end
--- lib/measurement.rb
+++ lib/measurement.rb
@@ -1,3 +1,15 @@
+# Measurement, all the units you need to describe and convert recipes.
+#
+# == Authors
+#
+# * Colin Harris (@aberant on twitter)
+#
+# == Copyright
+#
+# Copyright (c) 2010 Colin Harris
+#
+# This code released under the terms of the MIT license.
+
 require 'measurement/registry'
 require 'measurement/registry/unit_entry'
 require 'measurement/unit'
@@ -5,6 +17,9 @@ require 'measurement/core_ext/numeric'
 require 'measurement/core_ext/symbol'
 
 module Measurement
+  VERSION = File.read( File.join( File.dirname( __FILE__ ), "..", "VERSION"))
+
+
   class InvalidUnitError < StandardError ; end
   class InvalidAliasError < StandardError ; end
 end
