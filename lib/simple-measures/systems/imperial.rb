9d45102c0b80fdbb8eb4f70b60b36d8551064199
aberant <qzzzq1@gmail.com>
2010-08-15 13:18:24 -0500

    adding cups

--- lib/simple-measures/systems/imperial.rb
+++ lib/simple-measures/systems/imperial.rb
@@ -16,13 +16,14 @@ SimpleMeasures::Registry.add_alias( :ounce, :oz )
 # = Volume =
 # ==========
 SimpleMeasures::Registry.add_unit( :fluid_ounce, 29.5735296, :volume )
+SimpleMeasures::Registry.add_unit( :cup, 236.588237, :volume )
 SimpleMeasures::Registry.add_unit( :pint, 473.176473, :volume )
 SimpleMeasures::Registry.add_unit( :quart, 946.352946, :volume )
 SimpleMeasures::Registry.add_unit( :gallon, 3_785.41178, :volume )
 
 SimpleMeasures::Registry.add_alias( :fluid_ounce, :floz )
 SimpleMeasures::Registry.add_alias( :fluid_ounce, :ozfl )
-
+SimpleMeasures::Registry.add_alias( :cup, :cups )
 SimpleMeasures::Registry.add_alias( :pint, :pints )
 SimpleMeasures::Registry.add_alias( :quart, :quarts )
 SimpleMeasures::Registry.add_alias( :quarts, :qt )
